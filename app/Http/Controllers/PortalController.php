9439f6c174045c4faa2b1e383d5e479662332596
tack13 <sub.fxxk@gmail.com>
2018-10-08 11:08:38 +0900

    [add] PHP ArtisanによりModelを作成
    # 下記Modekを作成
    * User.php
    * Userinformation.php
    * WorkInformation.php
    
    ## Modelに対応したテーブル名を指定

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -7,14 +7,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\User;
 use Illuminate\Http\Request;
 
 class PortalController extends Controller
 {
-    //
     public function index() {
+        // @TODO ログインユーザの情報を取得 ログイン機能未実装
+        $user = User::get(['user_id', 'user_type', 'group_id'])->where('user_id', '1')->toArray();
         return view('top', [
             'title' => 'Portal',
+            'user' => $user,
         ]);
     }
 }
---
+++ app/Model/UserInformation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace \Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserInformation extends Model
+{
+    protected $table = 'tbl_user_information';
+}
