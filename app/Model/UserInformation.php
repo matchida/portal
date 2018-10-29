367d645e3515906437fb013022ba66db0065be06
tack13 <sub.fxxk@gmail.com>
2018-10-29 20:44:25 +0900

    [rm] 不要なメソッドがあったため削除

--- app/Model/UserInformation.php
+++ app/Model/UserInformation.php
@@ -20,14 +20,4 @@ class UserInformation extends Model
      * @var 削除フラグ 0:無効 1:有効
      */
     const DEL_FLG_FALSE = 0;
-
-    /**
-     * Function getData
-     * ユーザ情報を取得
-     *
-     * @return array
-     */
-    public function getData() {
-        return array('user' => 'takahiro');
-    }
 }
